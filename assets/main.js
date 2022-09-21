f2b721899d6529db28b24d7e62edc558fb797e47
51Shades <91501217+51Shades@users.noreply.github.com>
2022-09-21 15:32:29 +0700

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+const nama = document.getElementById("name");
+const ttl = document.getElementById("tl");
+const tgl = document.getElementById("tgl");
+const jk = document.getElementById("jk");
+const alamat = document.getElementById("alamat");
+const alamat2 = document.getElementById("alamat");
+const telepon = document.getElementById("telepon")
+const form = document.getElementById("form")
+const lomba = document.getElementsByName("lomba")
+
+form.addEventListener("submit",function(){
+    
+    window.alert("berhasil submit")
+   
+})
+
